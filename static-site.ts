edf7aee48319c389d026668a609914f41e39a373
Ekaterina <fedotova490@gmail.com>
2023-11-12 14:16:36 +0100

    fix bucket name to be unique

--- static-site.ts
+++ static-site.ts
@@ -5,52 +5,67 @@ import * as iam from "@aws-cdk/aws-iam";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
 export class StaticSite extends Construct {
-    constructor(parent: Stack, name: string) {
-        super(parent, name);
+  constructor(parent: Stack, name: string) {
+    super(parent, name);
 
-        // create OAI - special CloudFront user identity that you can associate with your S3 bucket
-        const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI")
+    // create OAI - special CloudFront user identity that you can associate with your S3 bucket
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
 
-        // create an s3 bucket
-        const siteBucket = new s3.Bucket(this, "JSCCStaticBucket", {
-            bucketName: "nodejs-aws-shop-react",
-            websiteIndexDocument: "index.html",
-            publicReadAccess: false,
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
-        })
+    const bucketName = "online-shop-bucket-of-katia";
 
-        // add a policy statement to perform actions on this bucket (read access for cloudfront distribution)
-        siteBucket.addToResourcePolicy(new iam.PolicyStatement({
-            actions: ["s3:GetObject"],
-            resources: [siteBucket.arnForObjects("*")],
-            // entities to which permissions (actions) apply
-            principals: [new iam.CanonicalUserPrincipal(cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
-        }))
+    // create an s3 bucket
+    const siteBucket = new s3.Bucket(this, "JSCCStaticBucket", {
+      bucketName: bucketName,
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
-        // create cloudfront distribution
-        const distribution = new cloudfront.CloudFrontWebDistribution(this, "JSCC-distribution", {
-            originConfigs: [{
-                s3OriginSource: {
-                    s3BucketSource: siteBucket,
-                    originAccessIdentity: cloudFrontOAI,
-                },
-                // cache behaviour - how to cache different content types
-                behaviors: [{
-                    isDefaultBehavior: true
-                }]
-            }]
-        })
+    //add a policy statement to perform actions on this bucket (read access for cloudfront distribution)
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        // entities to which permissions (actions) apply
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
 
-        // deploy the bucket
-        new s3deploy.BucketDeployment(this, "JSCC-Bucket-Deployment", {
-            // folder in our repository with the entry file
-            sources: [s3deploy.Source.asset(".")],
-            // the bucket where the file should be deployed
-            destinationBucket: siteBucket,
-            distribution,
-            // after each deployment the files will be invalidated and the cache will be refreshed
-            distributionPaths: ["/*"]
+    // create cloudfront distribution
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "JSCC-distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudFrontOAI,
+            },
+            // cache behaviour - how to cache different content types
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
 
-        })
-    }
+    //deploy the bucket
+    new s3deploy.BucketDeployment(this, "JSCC-Bucket-Deployment", {
+      // folder in our repository with the entry file
+      sources: [s3deploy.Source.asset("./dist")],
+      // the bucket where the file should be deployed
+      destinationBucket: siteBucket,
+      distribution,
+      // after each deployment the files will be invalidated and the cache will be refreshed
+      distributionPaths: ["/*"],
+    });
+  }
 }
